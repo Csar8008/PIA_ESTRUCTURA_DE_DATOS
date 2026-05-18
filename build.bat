6a8a2c08a63e34106b27b30f5f8287f3693a4521
Sebastián Calderon <sebastiancalderoncarrillo@gmail.com>
2026-05-18 17:17:35 -0600

    small tweaks

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 mkdir bin
-gcc .\src\dijkstra.h .\src\dijkstra.c .\src\grafo.c .\src\grafo.h .\src\main.c -o .\bin\programa
+gcc .\src\dijkstra.h .\src\dijkstra.c .\src\grafo.c .\src\grafo.h .\src\main.c .\src\utils.c .\src\utils.h -o .\bin\programa
 start .\bin\programa.exe
