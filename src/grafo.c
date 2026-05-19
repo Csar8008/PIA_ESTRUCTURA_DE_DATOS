204498b4dd2fa70e8072d4466bb778b69ce6684d
Sebastián Calderon <sebastiancalderoncarrillo@gmail.com>
2026-05-18 20:06:19 -0600

    added manual de usuario

--- src/grafo.c
+++ src/grafo.c
@@ -101,7 +101,7 @@ Grafo *importarGrafo()
 {
     int i, j, tam;
     char buffer[50];
-    FILE *grafoArchivo = fopen("src/EjemploGrafoVuelos.txt", "r");
+    FILE *grafoArchivo = fopen("src/GrafoVuelos.txt", "r");
     Grafo *grafoTemp = NULL;
 
     if (!validarFormatoArchivo(grafoArchivo))
